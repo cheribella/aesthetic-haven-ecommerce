2a0804c6e270fa276d5af601201ca63535d208ee
HONEY BUNNY <ysabelpatriciaramos@gmail.com>
2023-12-23 08:39:56 +0800

    Teka inaantok ako

--- store/components/navbar.tsx
+++ store/components/navbar.tsx
@@ -2,20 +2,28 @@ import Link from "next/link";
 
 import Container from "@/components/ui/container";
 import MainNav from "@/components/main-nav";
+import NavbarActions from "@/components/navbar-actions";
 
-const Navbar = () => {
-    return ( 
-        <div className="border-b">
-            <Container>
-                <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-                <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-                    <p className="font-bold text-xl">Aesthetic Haven</p>
-                </Link>
-                <MainNav data={[]}/>
-                </div>
-            </Container>
+import getCategories from "@/actions/get-categories";
+
+export const revalidate = 0;
+
+const Navbar = async () => {
+  const categories = await getCategories();
+
+  return (
+    <div className="border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">Aesthetic Haven</p>
+          </Link>
+          <MainNav data={categories} />
+          <NavbarActions />
         </div>
-     );
-}
- 
+      </Container>
+    </div>
+  );
+};
+
 export default Navbar;
