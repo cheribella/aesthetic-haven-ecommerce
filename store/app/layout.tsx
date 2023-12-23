b59ae2173865ea0be7078cdf213438d8ca7daed7
HONEY BUNNY <ysabelpatriciaramos@gmail.com>
2023-12-23 08:23:35 +0800

    Navigation bar and Footer for the Store

---
+++ store/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from 'next'
+
+import { Urbanist } from 'next/font/google'
+
+import './globals.css'
+
+import Navbar from '@/components/navbar'
+import Footer from '@/components/footer'
+
+const font = Urbanist({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Aesthetic Haven',
+  description: 'Store',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Navbar />
+        {children}
+        <Footer />
+        </body>
+    </html>
+  )
+}
