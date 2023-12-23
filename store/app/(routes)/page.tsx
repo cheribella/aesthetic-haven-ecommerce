b4a1ec90d8979dfcc20ebfcd510d490ba5cb20a2
HONEY BUNNY <ysabelpatriciaramos@gmail.com>
2023-12-23 11:55:27 +0800

    Noche Buena - Started with get-products, left it unfinished

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -1,4 +1,6 @@
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 import Billboard from "@/components/ui/billboard";
 import Container from "@/components/ui/container";
@@ -6,6 +8,7 @@ import Container from "@/components/ui/container";
 export const revalidate = 0;
 
 const HomePage = async() => {
+    const products = await getProducts({ isFeatured: true});
     const billboard = await getBillboard("b58b961f-7982-4863-9532-62fc9dd14b95");
 
     return ( 
@@ -13,6 +16,9 @@ const HomePage = async() => {
             <div className="space-y-10 pb-10">
                 <Billboard
                 data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products} />
             </div>
         </Container>
      );
