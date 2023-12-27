1ef53f3b591d38483200f5647082a719516e5b3f
HONEY BUNNY <ysabelpatriciaramos@gmail.com>
2023-12-27 15:18:15 +0800

    Adjusted the margin between the items and the footer

--- store/app/(routes)/page.tsx
+++ store/app/(routes)/page.tsx
@@ -7,22 +7,20 @@ import Container from "@/components/ui/container";
 
 export const revalidate = 0;
 
-const HomePage = async() => {
-    const products = await getProducts({ isFeatured: true});
-    const billboard = await getBillboard("b58b961f-7982-4863-9532-62fc9dd14b95");
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("b58b961f-7982-4863-9532-62fc9dd14b95");
 
-    return ( 
-        <Container>
-            <div className="space-y-10 pb-10">
-                <Billboard
-                data={billboard}/>
-            </div>
-            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                <ProductList title="Featured Products" items={products} />
-            </div>
-        </Container>
-     );
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
 
-}
- 
 export default HomePage;
