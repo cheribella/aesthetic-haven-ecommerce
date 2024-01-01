f5920868b42ba69ef0b428686a9766427cff5688
HONEY BUNNY <ysabelpatriciaramos@gmail.com>
2024-01-01 18:31:18 +0800

    naiiyak na ako puro kami alis pero anyways: Added Modal Components

--- store/app/layout.tsx
+++ store/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from 'next'
 
-import { Urbanist } from 'next/font/google'
+import { Sen } from 'next/font/google'
 
 import './globals.css'
 
 import Navbar from '@/components/navbar'
 import Footer from '@/components/footer'
+import ModalProvider from '@/providers/modal-provider'
 
-const font = Urbanist({ subsets: ['latin'] })
+const font = Sen({ weight: '400', subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Aesthetic Haven',
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+        <ModalProvider />
         <Navbar />
         {children}
         <Footer />
