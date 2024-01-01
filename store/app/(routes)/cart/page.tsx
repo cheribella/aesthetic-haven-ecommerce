244f90741d2e2c0dead4de6182e547f0e1614b2a
HONEY BUNNY <ysabelpatriciaramos@gmail.com>
2024-01-01 19:16:51 +0800

    Updated the Add to Cart feature

---
+++ store/app/(routes)/cart/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import useCart from "@/hooks/use-cart";
+import Container from "@/components/ui/container";
+import CartItem from "./components/cart-item";
+
+const CartPage = () => {
+  const cart = useCart();
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8">
+          <h1 className="font-bold text-black text-3xl">Shopping Cart</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added to cart.</p>
+              )}
+              <ul>
+                {cart.items.map((item) => (
+                  <CartItem data={item} key={item.id} />
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default CartPage;
